1e249ea1acae9bbe57b5e619eac481049cd1e91d
sabrinacb <79062119+sabrinacb@users.noreply.github.com>
2021-04-16 15:59:33 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+function reproducir(){
+document.getElementById('video').play();
+
+}
+function parar() {
+    document.getElementById('video').pause();
+
+}
+function video1(){
+
+    document.getElementById('video').src = 'imagenes/video-01.mp4';
+
+}
+function video2(){
+    document.getElementById('video').src = 'imagenes/video-02.mp4';
+
+}
+
+
+
